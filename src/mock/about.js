054571d59b6a55fc12c77eeaa2c9136d07776cc6
dinglx3 <aladingmao@gmail.com>
2024-03-19 14:25:08 +0800

    bug修复

--- src/mock/about.js
+++ src/mock/about.js
@@ -1,6 +1,6 @@
-import Mock from 'mockjs';
-Mock.mock('/api/about', 'get', {
-  code: 0,
-  msg: '',
-  data: 'http://resume.yuanjin.tech/',
-});
+// import Mock from 'mockjs';
+// Mock.mock('/api/about', 'get', {
+//   code: 0,
+//   msg: '',
+//   data: 'http://resume.yuanjin.tech/',
+// });
